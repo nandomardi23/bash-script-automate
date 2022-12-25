6c7fa2fe43639b7d3591b259cd1e760129d1000c
nandomardi23 <fernandomardi1@gmail.com>
2022-12-25 22:00:22 +0700

    tambahan

--- setup-server.sh
+++ setup-server.sh
@@ -4,16 +4,17 @@ echo 'selamat datang automate install LEMP';
 sudo apt update;
 sudo apt upgrade -y;
 echo 'telah selesai update';
-
+sleep 4;
 echo "install nginx";
-var=$(ls /etc/nginx/)
-if [$var == true]
+
+if [-d  "/etc/nginx/"]
 then
-echo 'nginx sudah ada!!'
-sleep 2;
+    echo 'nginx sudah ada!!'
+    sleep 2;
 else
-sudo apt install nginx -y;
-systemctl enable nginx ;
-systemctl status nginx;
-echo "nginx sudah di install";
-sleep 5;
+    sudo apt install nginx -y;
+    systemctl enable nginx ;
+    systemctl status nginx;
+    echo "nginx sudah di install";
+    sleep 5;
+fi
