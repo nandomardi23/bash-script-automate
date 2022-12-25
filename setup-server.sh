b9e78a3c1ceb09307a69a6776ab4193c9de60f5d
nandomardi23 <fernandomardi1@gmail.com>
2022-12-25 21:57:36 +0700

    tambahan

--- setup-server.sh
+++ setup-server.sh
@@ -6,8 +6,14 @@ sudo apt upgrade -y;
 echo 'telah selesai update';
 
 echo "install nginx";
+var=$(ls /etc/nginx/)
+if [$var == true]
+then
+echo 'nginx sudah ada!!'
+sleep 2;
+else
 sudo apt install nginx -y;
 systemctl enable nginx ;
 systemctl status nginx;
-q;
 echo "nginx sudah di install";
+sleep 5;
