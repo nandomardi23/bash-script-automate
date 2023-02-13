86e03a5fa37d7bd7b07d0bd04a4d3c3751cec328
nandomardi23 <fernandomardi1@gmail.com>
2023-02-13 22:50:29 +0700

    perbaikan erorr

--- install-docker.sh
+++ install-docker.sh
@@ -1,29 +1,31 @@
-#!/bin/bash
+echo "check update";
+sudo apt-get update;
 
-sudo apt-get update
-sudo apt-get install
-    \ ca-certificates
-    \ curl
-    \ gnupg
-    \ lsb-release;
+echo "insall sesuatu yang dibutuhkan dalam menginstall";
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release;
 
-echo "instalation depend success";
+echo "membuat folder keyrings";
+sudo mkdir -m 0755 -p /etc/apt/keyrings;
 
-sudo mkdir -p /etc/apt/keyrings;
+echo "mendownload docker.gpg";
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y;
 
-echo "download docker key suceess";
-
- echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+echo "pindahkan docker.gpg ke ect/apt/sources.list.d";
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
 
-  sudo apt-get update;
+echo "sesuau yang dibutuhkan";
+sudo chmod a+r /etc/apt/keyrings/docker.gpg;
+sudo apt-get update;
+echo "download installan";
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
 
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y;
-
-  sudo groupadd docker;
-  sudo usermod -aG docker $USER;
-  echo "user mod udah";
-  sudo newgrp docker;
-  echo "membuat new group success"
-  docker run hello-world;
+echo "buat dockernya bisa diakses tanpa sudo";
+sudo usermod -aG docker $USER;
+newgrp docker;
+docker run hello-world;
